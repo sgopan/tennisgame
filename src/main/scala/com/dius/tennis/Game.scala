fb2010c74acf4291cb09aab316d8dafd61f6b57c
Gopan Sugathan <gopancd@gmail.com>
2017-01-22 22:21:31 +1100

    Added some more comments to code

--- src/main/scala/com/dius/tennis/Game.scala
+++ src/main/scala/com/dius/tennis/Game.scala
@@ -15,8 +15,7 @@ object Game {
   sealed trait Status
 
   /**
-    * Represents a Player in a game.
-    *
+    * Represents a Player in a game.    *
     * @param name   - The name of the player
     * @param point  - The point in a game (0,15, 30, 40, 60)
     * @param status - Its a Deuce, Advantage or None(default)
@@ -24,8 +23,7 @@ object Game {
   case class Player(name: String, var point: Int = 0, var status: Option[Status] = None)
 
   /**
-    * Represents Score in a Game
-    *
+    * Represents Score in a Game    *
     * @param player1 - The first player  object
     * @param player2 - - The second player  object
     */
@@ -42,9 +40,11 @@ object Game {
 /**
   * This class represents a game in a tennis. The rules of the game are described below
   * A game is won by the first player to have won at least four points in total and at least two points more than the opponent.
-  * The running score of each game is described in a manner peculiar to tennis: scores from zero to three points are described as 0, 15, 30, 40, respectively
+  * The running score of each game is described in a manner peculiar to tennis: scores from zero to three points are described
+  * as 0, 15, 30, 40, respectively.
   * If at least three points have been scored by each player, and the scores are equal, the score is "deuce".
-  * If at least three points have been scored by each side and a player has one more point than his opponent, the score of the game is "advantage" for the player in the lead.
+  * If at least three points have been scored by each side and a player has one more point than his opponent, the score of the 
+  * game is "advantage" for the player in the lead.
   *
   * @param player1 - The name of the first player
   * @param player2 - The name of the second player
@@ -62,8 +62,7 @@ class Game(player1: String, player2: String) {
 
 
   /**
-    * This method will update the current score
-    *
+    * This method will update the current score    *
     * @param player - The name of the player who won the point
     */
   def pointWonBy(player: String): Unit = {
@@ -84,34 +83,34 @@ class Game(player1: String, player2: String) {
   private def updateScoreFor1stPlayer(): Unit = {
 
     this.gameScore match {
-
-      case Score(Player(_, 30, None), Player(_, 40, None)) => //Player 1 entering Deuce.
+      //Player 1 entering Deuce.
+      case Score(Player(_, 30, None), Player(_, 40, None)) => 
         this.gameScore.player1.point = this.gameScore.player1.point + INCREMENT_BY_TEN
         this.gameScore.player1.status = Some(Deuce)
         this.gameScore.player2.status = Some(Deuce)
 
-
-      case Score(Player(_, 40, Some(Deuce)), Player(_, 40, Some(Deuce))) => //Player1 entering Advantage.
+      //Player1 entering Advantage.  
+      case Score(Player(_, 40, Some(Deuce)), Player(_, 40, Some(Deuce))) => 
         this.gameScore.player1.status = Some(Advantage)
         this.gameScore.player2.status = None
 
-
-      case Score(Player(_, 40, None), Player(_, 40, Some(Advantage))) => //Player2 is in Advantage state. Player1 wins a point and status returns to Deuce
+      //Player2 is in Advantage state. Player1 wins a point and status returns to Deuce
+      case Score(Player(_, 40, None), Player(_, 40, Some(Advantage))) => 
         this.gameScore.player1.status = Some(Deuce)
         this.gameScore.player2.status = Some(Deuce)
 
-
-      case Score(Player(_, 40, Some(Advantage)), Player(_, 40, None)) => //Player1 is in Advantage  state. Player1 wins a point and wins the game
+      //Player1 is in Advantage  state. Player1 wins a point and wins the game
+      case Score(Player(_, 40, Some(Advantage)), Player(_, 40, None)) => 
         this.gameScore.player1.point = this.gameScore.player1.point + INCREMENT_BY_TWENTY
         this.gameScore.player1.status = None
         this.gameScore.player2.status = None
 
-
-      case Score(Player(_, 30, None), _) => this.gameScore.player1.point += INCREMENT_BY_TEN // If current point is 30 increment by 10
-
-      case Score(Player(_, 40, None), _) => this.gameScore.player1.point += INCREMENT_BY_TWENTY // If current point is 40 increment by 20
-
-      case _ => this.gameScore.player1.point += INCREMENT_BY_FIFTEEN //Any Other case simply increment the point
+      // If current point is 30 increment by 10
+      case Score(Player(_, 30, None), _) => this.gameScore.player1.point += INCREMENT_BY_TEN 
+      // If current point is 40 increment by 20
+      case Score(Player(_, 40, None), _) => this.gameScore.player1.point += INCREMENT_BY_TWENTY 
+      //Any Other case simply increment the point
+      case _ => this.gameScore.player1.point += INCREMENT_BY_FIFTEEN 
 
 
     }
@@ -123,52 +122,51 @@ class Game(player1: String, player2: String) {
   private def updateScoreFor2ndPlayer(): Unit = {
 
     this.gameScore match {
-
-      case Score(Player(_, 40, None), Player(_, 30, None)) => //Player 2 entering Deuce.
+      //Player 2 entering Deuce.
+      case Score(Player(_, 40, None), Player(_, 30, None)) => 
         this.gameScore.player2.point = this.gameScore.player2.point + INCREMENT_BY_TEN
         gameScore.player2.status = Some(Deuce)
         gameScore.player1.status = Some(Deuce)
 
-
-      case Score(Player(_, 40, Some(Deuce)), Player(_, 40, Some(Deuce))) => //Player2 entering Advantage.
+      //Player2 entering Advantage.
+      case Score(Player(_, 40, Some(Deuce)), Player(_, 40, Some(Deuce))) => 
         this.gameScore.player2.status = Some(Advantage)
         this.gameScore.player1.status = None
 
-
-      case Score(Player(_, 40, Some(Advantage)), Player(_, 40, None)) => //Player1 is in Advantage state. Player2 wins a point and status returns to Deuce
+      //Player1 is in Advantage state. Player2 wins a point and status returns to Deuce
+      case Score(Player(_, 40, Some(Advantage)), Player(_, 40, None)) => 
         this.gameScore.player1.status = Some(Deuce)
         this.gameScore.player2.status = Some(Deuce)
 
-
-      case Score(Player(_, 40, None), Player(_, 40, Some(Advantage))) => //Player2 is in Advantage  state. Player2 wins a point and wins the game
+      //Player2 is in Advantage  state. Player2 wins a point and wins the game
+      case Score(Player(_, 40, None), Player(_, 40, Some(Advantage))) => 
         this.gameScore.player2.point = this.gameScore.player2.point + INCREMENT_BY_TWENTY
         this.gameScore.player2.status = None
         this.gameScore.player1.status = None
 
-
-      case Score(_, Player(_, 30, None)) => this.gameScore.player2.point += INCREMENT_BY_TEN //If current point is 30 increment by 10
-
-      case Score(_, Player(_, 40, None)) => this.gameScore.player2.point += INCREMENT_BY_TWENTY // If current point is 40 increment by 20
-
-      case _ => this.gameScore.player2.point = this.gameScore.player2.point + INCREMENT_BY_FIFTEEN //Any Other case simply increment th point
+      //If current point is 30 increment by 10
+      case Score(_, Player(_, 30, None)) => this.gameScore.player2.point += INCREMENT_BY_TEN 
+      // If current point is 40 increment by 20
+      case Score(_, Player(_, 40, None)) => this.gameScore.player2.point += INCREMENT_BY_TWENTY 
+      //Any Other case simply increment th point
+      case _ => this.gameScore.player2.point = this.gameScore.player2.point + INCREMENT_BY_FIFTEEN 
 
     }
   }
 
   /**
-    * This method will return the current score for the tennis game
-    *
+    * This method will return the current score for the tennis game.
     * @return - current score
     */
   def score(): String = {
 
     this.gameScore match {
 
-      case Score(Player(_, _, Some(Deuce)), Player(_, _, Some(Deuce))) => "Deuce"
+      case Score(Player(_, 40, Some(Deuce)), Player(_, 40, Some(Deuce))) => "Deuce"
 
-      case Score(Player(player, _, Some(Advantage)), _) => "Advantage " + player
+      case Score(Player(player, 40, Some(Advantage)), _) => "Advantage " + player
 
-      case Score(_, Player(player, _, Some(Advantage))) => "Advantage " + player
+      case Score(_, Player(player, 40, Some(Advantage))) => "Advantage " + player
 
       case Score(Player(player, 60, None), _) => player + " wins"
 
